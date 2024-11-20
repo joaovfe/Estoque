46fc41413f0d282a0b87e9042b4ab455e82281c3
Joao Espindola <joaovitorfespindola@gmail.com>
2024-11-20 19:14:43 -0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AddJoias;
 use App\Http\Controllers\DeleteJoia;
+use App\Http\Controllers\DetalheItem;
 use App\Http\Controllers\ListagemJoias;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::get('/', [ListagemJoias::class, 'listarJoias'])->name('home');
 Route::get('/AdicionarJoia', [AddJoias::class, 'addJoias']);
 Route::post('/addJoia', [AddJoias::class, 'salvarJoias']);
 Route::post('/deleteJoia', [DeleteJoia::class, 'deleteJoia']);
+Route::get('/detalheJoia/{id}', [DetalheItem::class, 'detalheItem'])->name('detalheJoia');
+Route::put('/detalheJoia/{id}', [DetalheItem::class, 'salvarJoia'])->name('updateJoia');
