0670eab42eb68cfc52ed693669a2504f8f032ee9
Joao Espindola <joaovitorfespindola@gmail.com>
2024-11-20 16:04:38 -0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AddJoias;
 use App\Http\Controllers\ListagemJoias;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ListagemJoias::class, 'listarJoias']);
+Route::get('/AdicionarJoia', [AddJoias::class, 'addJoias']);
+Route::post('/addJoia', [AddJoias::class, 'salvarJoias']);
